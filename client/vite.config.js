fe941e79e7bde5b6166a3439080d258a1e54741e
instinct28 <yk267164@gmail.com>
2024-10-24 01:42:09 +0530

    Completed

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174, // Set the port to 5173
+    host: '0.0.0.0',  // Allow external access
+    port: 5174,       // Your specified port
   },
 });
